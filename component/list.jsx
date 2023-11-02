1f33feb6ecf2c6cbe1a1f04873079fac362ed32b
Bilguudei <bilguudei.shinezul23@gmail.com>
2023-11-02 15:41:33 +0800

    Edit

--- component/list.jsx
+++ component/list.jsx
@@ -1,13 +1,30 @@
 import Image from "next/image";
 import "./list.css";
-
+import { useState } from "react";
 const List = ({ name }) => {
+  const [text, setText] = useState(name);
+  const [edit, setEdit] = useState(false);
+  const Edit = (e) => {
+    e.preventDefault();
+    setEdit(!edit);
+  };
   return (
     <div className="container">
       <input type="checkbox" className="check-box" />
-      <div>{name}</div>
+      {edit ? (
+        <form onSubmit={Edit}>
+          <input
+            autoFocus
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </form>
+      ) : (
+        <div>{text}</div>
+      )}
       <div className="icons">
         <Image
+          onClick={Edit}
           src="/edit.svg"
           alt="edit icon"
           className="image"
