da4fb64e14272a52e36d28b461a5d0bd39da180a
Amarjargal Ayurzana <akiko@Amarjargals-MacBook-Pro.local>
2023-11-02 00:19:56 -0700

    add

--- app/page.js
+++ app/page.js
@@ -1,11 +1,45 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import List from '../component/list'
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import List from "../component/list";
+import { useState } from "react";
+
 export default function Home() {
-  return (<div style = {{display:'flex', alignItems:'center', height: '100vh', flexDirection: 'column', justifyContent: 'center'}}>
-    <List name = "sda"/>
-    <List/>
-    <List/>
-    <List/>
-  </div>);
+  const [list, setList] = useState([{ name: "sda", status: "checked" }]);
+  const [add, setAdd] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted!");
+    const temp = [...list];
+    temp.push({ name: add });
+    setList(temp);
+    setAdd("");
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Add"
+            value={add}
+            onChange={(e) => setAdd(e.target.value)}
+          />
+        </form>
+      </div>
+      {list.map(({ name, status }) => {
+        return <List name={name} status={status} />;
+      })}
+    </div>
+  );
 }
--- component/list.jsx
+++ component/list.jsx
@@ -1,12 +1,35 @@
+import Image from "next/image";
 import "./list.css";
-const List = ({name}) => {
-    return (
-        <div>
-            <div className="container">
-                <input type = "checkbox"/>
-                {name}
-            </div>
-        </div>
-    );
-}
+
+const List = ({ name }) => {
+  return (
+    <div className="container">
+      <input type="checkbox" className="check-box" />
+      <div>{name}</div>
+      <div className="icons">
+        <Image
+          src="/edit.svg"
+          alt="edit icon"
+          className="image"
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/delete.svg"
+          alt="drag icon"
+          className="image"
+          width={20}
+          height={20}
+        />
+        <Image
+          src="/drag.svg"
+          alt="drag icon"
+          className="image"
+          width={20}
+          height={20}
+        />
+      </div>
+    </div>
+  );
+};
 export default List;
